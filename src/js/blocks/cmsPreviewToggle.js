3e14640ead512aeda6b06d98d2257e746dc8c470
Lero <weblero2020@gmail.com>
2025-05-05 10:25:45 +0300

    fix build

--- src/js/blocks/cmsPreviewToggle.js
+++ src/js/blocks/cmsPreviewToggle.js
@@ -1,8 +1,10 @@
 export default function initCmsPreviewToggle() {
-  document.querySelector('.js-cms-preview-toggle').addEventListener('click', (e) => {
-    e.preventDefault();
-    e.currentTarget.classList.toggle('is-active');
-    const cmsPanel = document.querySelector('.cms-preview');
-    cmsPanel.classList.toggle('is-open');
-  });
+  document
+    .querySelector('.js-cms-preview-toggle')
+    .addEventListener('click', (e) => {
+      e.preventDefault();
+      e.currentTarget.classList.toggle('is-active');
+      const cmsPanel = document.querySelector('.cms-preview');
+      cmsPanel.classList.toggle('is-open');
+    });
 }
